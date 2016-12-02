149fbabf3e95ffb9527102a911f56ee43273b445
erkand-imeri <erkand.imeri@gmail.com>
2016-12-02 09:42:03 +0100

    ipaddress

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ var express=require('express');
 var app=express();
 
 app.get('/',function(req,res){
-res.send("Request Header Parser!");
+
+var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+
+
+res.json({
+    ipaddress: ip
 });
+});
+
+
+
 
 app.listen(process.env.PORT || 8080, function () {
   console.log('Example app listening on port 8080!');

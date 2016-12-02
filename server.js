0a7a8327dcb2fc1cefed790c802f1e34c598f0fc
erkand-imeri <erkand.imeri@gmail.com>
2016-12-02 09:17:19 +0100

    server listening

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("I am a Server");
+var express=require('express');
+var app=express();
+
+app.get('/',function(req,res){
+res.send("Request Header Parser!");
+});
+
+app.listen(process.env.PORT || 8080, function () {
+  console.log('Example app listening on port 8080!');
+});
